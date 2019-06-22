ca61cda4812435d5dd6c37c0bdd0f184f117588d
Melvin Varghese <36334526+melvinvarghese@users.noreply.github.com>
2019-06-22 19:20:52 +0530

    Add files via upload

--- hosts/download/solidfiles_com.php
+++ hosts/download/solidfiles_com.php
@@ -1,28 +1,40 @@
 <?php
 
 if (!defined('RAPIDLEECH')) {
-	require_once('index.html');
-	exit;
+    require_once 'index.html';
+    exit;
 }
 
+class solidfiles_com extends DownloadClass
+{
+    public function Download($link)
+    {
+        if (!preg_match('/solidfiles\.com\/v\/(\w+)/', $link, $link)) {
+            html_error('Invalid download link');
+        }
+        $link = $link[1];
+        $ch = curl_init('http://www.solidfiles.com/v/'.$link);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_COOKIEJAR, 'solidfiles_cookie.txt');
+        curl_setopt($ch, CURLOPT_COOKIEFILE, 'solidfiles_cookie.txt');
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.7) Gecko/20070914 Firefox/2.0.0.7');
+        $csrf = curl_exec($ch);
+        if (!preg_match('/\'csrfmiddlewaretoken\' value=\'(.*?)\'/', $csrf, $csrf)) {
+            html_error('File not found.');
+        }
+        $csrf = $csrf[1];
+        curl_setopt($ch, CURLOPT_URL, 'http://www.solidfiles.com/v/'.$link.'/dl');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, ['csrfmiddlewaretoken' => $csrf, 'referrer' => '']);
+        $dl = curl_exec($ch);
+        curl_close($ch);
+        preg_match('/"downloadUrl":"(.*?)"/u', $dl, $dl);
+        $dl = $dl[1];
 
-class solidfiles_com extends DownloadClass {
-
-	public function Download($link) {
-		
-		echo 'Solidfiles.com Download Plugin by <b>The Devil</b>';
-		$this->page = $this->GetPage($link);
-		is_present($this->page,'The page you were looking for appears to no longer be there','File Unavailable');
-		$devil = preg_match('~"downloadUrl":"(.*)"~U',$this->page,$dlocs);
-		(!$devil)?html_error('[0]Error: Unable to Find Download Location'):'';
-		is_notpresent($dlocs[1],'solidfilesusercontent.com','[1]Error: Check FileHost CDN');
-		return $this->RedirectDownload($dlocs[1], urldecode(basename(parse_url($dlocs[1], PHP_URL_PATH))));
-	}
-	
+        return $this->RedirectDownload($dl, urldecode(basename(parse_url($dl, PHP_URL_PATH))));
+    }
 }
 
 // Written by The Devil
 // [2016-06-09] Code Clean Up
 // [2017-04-16] Fixed Download Regex
-
-?>
+// [2018-06-18] Fixed by NimaH79
--- hosts/download/uptostream_com.php
+++ hosts/download/uptostream_com.php
@@ -1,30 +1,35 @@
 <?php
 
 if (!defined('RAPIDLEECH')) {
-	require_once('index.html');
-	exit();
+    require_once 'index.html';
+    exit();
 }
 
-class uptostream_com extends DownloadClass {
-	public function Download($link) {
-		if (!preg_match('@https?://(?:[\w\-]+\.)+[\w\-]+(?:\:\d+)?/(\w{12})(?=(?:[/\.]|(?:\.html?))?)@i', str_ireplace('/embed-', '/', $link), $link)) html_error('Invalid link?.');
-		$vid = $link[1];
-		$link = $link[0];
+class uptostream_com extends DownloadClass
+{
+    public function Download($link)
+    {
+        if (!preg_match('/http?s:\/\/(?:[\w\-]+\.)+[\w\-]+(?:\:\d+)?\/(\w{12})(?=(?:[\/\.]|(?:\.html?))?)/i', str_ireplace('/embed-', '/', $link))) {
+            html_error('Invalid link?');
+        }
 
-		$page = $this->GetPage($link);
-		is_present($page, '404 (File not found)', 'File Not Found');
+        $page = $this->GetPage($link);
+        is_present($page, '404 (File not found)', 'File Not Found');
 
-		if (!preg_match('@<title>(?>(.*?)</title>)@is', $page, $title)) html_error('Error: Video title not found.');
-		if (!preg_match('@https?://(?:[\w\-]+\.)+[\w\-]+(?:\:\d+)?/\w+/(\d+)/0@i', $page, $DL)) html_error('Download link not found.');
+        if (!preg_match('/var filename = \'(.*?)\'/', $page, $title)) {
+            html_error('Error: Video title not found.');
+        }
+        $title = $title[1];
 
-		$filename = preg_replace('@[^ A-Za-z_\-\d\.,\(\)\[\]\{\}&\!\'\@\%\#]@u', '_', html_entity_decode(trim($title[1]), ENT_QUOTES, 'UTF-8'));
-		$filename = preg_replace('@(?:\.(?:mp4|flv|mkv|webm|wmv|(m2)?ts|rm(vb)?|mpe?g?|vob|avi|[23]gp))+$@i', '', $filename);
-		$filename .= " [UTS-{$DL[1]}p][$vid].mp4";
+        if (!preg_match('/"src":"(.*?)"/', $page, $DL)) {
+            html_error('Download link not found.');
+        }
+        $DL = $DL[1];
+        $DL = str_replace('\\', '', $DL);
 
-		$this->RedirectDownload($DL[0], $filename, 0, 0, 0, $filename);
-	}
+        $this->RedirectDownload($DL, $filename, 0, 0, 0, $filename);
+    }
 }
 
 // [25-12-2015] Written by Th3-822.
-
-?>
+// [28-08-2018] Updated by NimaH79.
